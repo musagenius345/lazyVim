7eea0c2a1f1e13d5f08b200b2eb7a43e326c575e
musagenius345 <musabadru@gmail.com>
2023-10-16 02:21:07 +0300

    commenting plugin replace comment.nvim

---
+++ lua/plugins/mini-comment.lua
@@ -0,0 +1,25 @@
+--{
+return
+--{
+{
+  "echasnovski/mini.comment",
+  opts = {
+    mappings = {
+      comment_line = "<leader>/",
+    },
+  },
+  config = function(_, opts)
+    vim.schedule(function()
+      require("mini.comment").setup(opts)
+    end)
+  end,
+}
+
+--, {
+--     'numToStr/Comment.nvim',
+--     opts = {
+--         -- add any options here
+--     },
+--     lazy = false,
+-- }
+--}
