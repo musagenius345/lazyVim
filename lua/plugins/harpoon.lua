3e2809778caf870880da49c083db60fdd3048873
musagenius345 <musabadru@gmail.com>
2023-12-05 13:02:12 +0300

    backup

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,18 @@
+--local harpoon = require("harpoon")
+
+return {
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  requires = { { "nvim-lua/plenary.nvim" } },
+  --     opts = {
+  --
+  -- keys = {
+  --       { "<leader>a", function() harpoon:list():append() end), desc='Add to harpoon'},
+  --   {"<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)},
+  --   { "<C-h>", function() harpoon:list():select(1) end)},
+  --   {"<C-t>", function() harpoon:list():select(2) end), desc='select 2nd'},
+  --   {"<C-n>", function() harpoon:list():select(3) end)},
+  --   {"<C-s>", function() harpoon:list():select(4) end)}
+  --   }
+  --   }
+}
